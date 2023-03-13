08b3d738e8c974e405e6f743bc964c78a65283c6
danieldb <danieldbrant@gmail.com>
2023-03-14 07:39:48 +0800

    Fixed Script

--- aws-terraform/templates/linux.sh
+++ aws-terraform/templates/linux.sh
@@ -6,3 +6,5 @@ sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo docker pull danieldbrant/node-server:node-1.0
+docker run -d -p 3000:3000 danieldbrant/node-server:node-1.0
+
